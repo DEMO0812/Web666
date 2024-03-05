3f01fd78d1fb649bee7b3db562b4fdb10fed09ed
Evie1911 <161868520+Evie1911@users.noreply.github.com>
2024-03-05 11:20:45 +0700

    Account.java

--- Web666/Web666/src/main/java/com/poly/been/Account.java
+++ Web666/Web666/src/main/java/com/poly/been/Account.java
@@ -6,6 +6,7 @@ import java.util.List;
 import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.Id;
 import javax.persistence.OneToMany;
 import javax.persistence.Table;
@@ -44,12 +45,81 @@ public class Account implements Serializable{
     @Column(name = "photo")
     private String photo;
 
-    @OneToMany(mappedBy = "account", cascade = CascadeType.ALL)
+    @OneToMany(mappedBy = "account", fetch = FetchType.EAGER)
     @JsonIgnore
     private List<Authority> authorities;
-
-    @OneToMany(mappedBy = "account", cascade = CascadeType.ALL)
     @JsonIgnore
-    private List<FavoriteProduct> favoriteProducts;
-
+	@OneToMany(mappedBy = "account")
+	List<Order> orders;
+	public Account(String username, String password, String fullname, String email, String account_adress, String phone,
+			String photo, List<Authority> authorities, List<Order> orders) {
+		super();
+		this.username = username;
+		this.password = password;
+		this.fullname = fullname;
+		this.email = email;
+		this.account_adress = account_adress;
+		this.phone = phone;
+		this.photo = photo;
+		this.authorities = authorities;
+		this.orders = orders;
+	}
+	public Account() {
+		super();
+	}
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public String getFullname() {
+		return fullname;
+	}
+	public void setFullname(String fullname) {
+		this.fullname = fullname;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getAccount_adress() {
+		return account_adress;
+	}
+	public void setAccount_adress(String account_adress) {
+		this.account_adress = account_adress;
+	}
+	public String getPhone() {
+		return phone;
+	}
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+	public String getPhoto() {
+		return photo;
+	}
+	public void setPhoto(String photo) {
+		this.photo = photo;
+	}
+	public List<Authority> getAuthorities() {
+		return authorities;
+	}
+	public void setAuthorities(List<Authority> authorities) {
+		this.authorities = authorities;
+	}
+	public List<Order> getOrders() {
+		return orders;
+	}
+	public void setOrders(List<Order> orders) {
+		this.orders = orders;
+	}
+   
 }
