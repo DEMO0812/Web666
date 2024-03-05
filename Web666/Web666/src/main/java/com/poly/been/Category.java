e68d849410b167d9b50358e1ae4aad12a9760d32
Evie1911 <161868520+Evie1911@users.noreply.github.com>
2024-03-05 11:22:11 +0700

    Category.java

--- Web666/Web666/src/main/java/com/poly/been/Category.java
+++ Web666/Web666/src/main/java/com/poly/been/Category.java
@@ -34,4 +34,40 @@ public class Category implements Serializable {
 	@JsonIgnore
 	private List<Product> products;
 
+	public String getCateId() {
+		return cateId;
+	}
+
+	public void setCateId(String cateId) {
+		this.cateId = cateId;
+	}
+
+	public String getCateName() {
+		return cateName;
+	}
+
+	public void setCateName(String cateName) {
+		this.cateName = cateName;
+	}
+
+	public List<Product> getProducts() {
+		return products;
+	}
+
+	public void setProducts(List<Product> products) {
+		this.products = products;
+	}
+
+	public Category(String cateId, String cateName, List<Product> products) {
+		super();
+		this.cateId = cateId;
+		this.cateName = cateName;
+		this.products = products;
+	}
+
+	public Category() {
+		super();
+	}
+
+	
 }
